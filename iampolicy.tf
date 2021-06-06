de22e6c171c4f68d100e2e8f18c5ccd1ca4a85be
chand <chand.nalubolu@gmail.com>
2021-06-06 19:40:22 +0530

    IAM attachment module

---
+++ iampolicy.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_policy" "policy" {
+  name        = "${var.policyname}"
+  path        = "/"
+  description = "My test policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*",
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  roles      = ["${var.attachroles}"]
+  policy_arn = aws_iam_policy.policy.arn
+}
